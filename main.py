785951943d723a7f439ad721514969acb0092b1c
alves-Moises <moisesinho_ra@hotmal.com>
2021-09-21 18:45:36 -0300

    update: espacements

--- main.py
+++ main.py
@@ -17,14 +17,13 @@ def valid_int():
 #valodacao retorno booleano
 def valid_int_bool(valores):
     valid = False
-    while not valid:
-        try:
-            int(valores)
+    try:
+        int(valores)
     except ValueError:
         print('erro em ', valores)
     else:
         valid = True 
-    print('valido')
+    return valid
 
 #Rollagem de dados
 def roll(dices, dices_list = []):
@@ -37,7 +36,7 @@ def nova_lista(lista):
     valid = False
     posicoes = [0]
 
-    while not (valid or (max(posicoes) <= max(lista))):
+    while not (valid or (max(posicoes) > len(lista))):
         if posicoes != [0]:
             print('Digite posições válidas')
 
@@ -50,6 +49,7 @@ def nova_lista(lista):
                 valid = False
 
 
+    return posicoes   
 
 
 #funcao principal
@@ -72,7 +72,7 @@ def main():
             resultado = roll(dados_qtd, list_dados)
             
             for j in range(len(resultado)): 
-                print(f'Dado n{j}: ', resultado[i])
+                print(f'Dado n{j}: ', resultado[j])
 
             #resultado ordenado
             print('#' * 50)
@@ -83,7 +83,6 @@ def main():
 
 
             i += 1
-        
 
         
 main()
