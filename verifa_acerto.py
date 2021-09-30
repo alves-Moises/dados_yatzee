5a4b8fa702770d111b0eb6fcb607080145f9938f
alves-Moises <moisesinho_ra@hotmail.com>
2021-09-30 12:04:34 -0300

    Update: print(escolha)

--- verifa_acerto.py
+++ verifa_acerto.py
@@ -32,13 +32,21 @@ def escolha_lista():
 #printa tabela de pontuacoes
 def print_escolha():
     print('Digite sua escolha:')
-    print('# [1] 1')
-    print('# [2] 2')
-    print('# [3] 3')
-    print('# [4] 4')
-    print('# [5] 5')
-    print('# [6] 6')
-
+    print('# [1]   1')
+    print('# [2]   2')
+    print('# [3]   3')
+    print('# [4]   4')
+    print('# [5]   5')
+    print('# [6]   6')
+    print('# [t]   Trinca')
+    print('# [pp]  pôker')
+    print('# [23x] 2x-3x')
+    print('# [es]  escada')
+    print('# [ec]  cheia')
+    print('# [ytz] yatzee 1 (111)')
+    print('# [yy]  yazzee 2')
+    print('# [0]   chance')
+    print('Sua escolha: ', end='')
 #atualizar_pontuacao
 def registra_pontuacao(lista, jogador):
     print_escolha()
