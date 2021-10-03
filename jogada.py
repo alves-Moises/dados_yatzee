8ac68944b41c03ec20b90ca1c34ecc906461907a
alves-Moises <moisesinho_ra@hotmail.com>
2021-10-02 23:07:01 -0300

    new function: valid_int()

--- jogada.py
+++ jogada.py
@@ -1,4 +1,14 @@
 
+def valid_int():
+    valid = False
+    while not valid:
+        try:
+            x = int(input())
+        except:
+            print('Digite um valor válido.')
+        else:
+            valid = True
+    return x
 
 def escolha_num(lista, jogador, escolha):
     jogador[escolha] = lista.count(int(escolha)) * int(escolha)
